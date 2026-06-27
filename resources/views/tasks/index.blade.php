90da10b66a9dee98112893e8091671f30ab2eea4
hzokbe <hzokbe@gmail.com>
2026-06-27 19:58:03 -0300

    feat(tasks): create task

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,6 +3,9 @@
 @section('code')
     <div class="card">
         <h1>Tasks</h1>
+        <div class="mb-2 mt-2">
+            <a href="{{ route('tasks.create') }}">Create task</a>
+        </div>
         @foreach($tasks as $task)
             <div class="task-card">
                 <h2>{{$task->title}}</h2>
