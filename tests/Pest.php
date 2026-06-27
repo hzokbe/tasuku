0d8b83e1af399b9ea7cb1540da90457e07c08df7
hzokbe <hzokbe@gmail.com>
2026-06-27 19:55:01 -0300

    chore: initial commit

---
+++ tests/Pest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Tests\TestCase;
+
+pest()->extend(TestCase::class)->in('Feature');
+
+expect()->extend('toBeOne', function () {
+    return $this->toBe(1);
+});
+
+function something()
+{
+}
