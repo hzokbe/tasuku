e0a41177b891772fe6f685c393b7d9d1d2bd8d76
hzokbe <hzokbe@gmail.com>
2026-06-27 22:15:33 -0300

    feat(tasks): delete task

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,8 +5,12 @@
         <h1>{{$task->title}}</h1>
         <p>{{$task->description}}</p>
     </div>
-    <div class="mt-2 mx-4">
+    <div class="mt-2 mx-4 flex gap-2">
         <button type="button" onclick="window.location.href='{{ route('tasks.edit', $task) }}'">Edit task</button>
+        <form action="{{ route('tasks.destroy', $task) }}" method="POST">
+            @csrf @method('DELETE')
+            <button class="destroy-button" type="submit">Destroy task</button>
+        </form>
         <button type="button" onclick="window.location.href='{{ route('tasks.index') }}'">Back</button>
     </div>
 @endsection
