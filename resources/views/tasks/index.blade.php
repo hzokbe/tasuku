1b4798c17d87a4f60e0768c0fdc0eddad75b2b09
hzokbe <hzokbe@gmail.com>
2026-06-27 22:02:51 -0300

    feat(tasks): update task

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,12 +4,19 @@
     <div class="card">
         <h1>Tasks</h1>
         <div class="mb-2 mt-2">
-            <a href="{{ route('tasks.create') }}">Create task</a>
+            <a class="button" href="{{ route('tasks.create') }}">Create task</a>
         </div>
         @foreach($tasks as $task)
-            <div class="task-card">
-                <h2>{{$task->title}}</h2>
-                <p>{{$task->description}}</p>
+            <div class="flex items-center task-card">
+                <div class="flex-1">
+                    <h2>
+                        <a href="{{ route('tasks.show', $task) }}">{{$task->title}}</a>
+                    </h2>
+                    <p>{{$task->description}}</p>
+                </div>
+                <div>
+                    <a class="button" href="{{ route('tasks.edit', $task) }}">Edit task</a>
+                </div>
             </div>
         @endforeach
     </div>
